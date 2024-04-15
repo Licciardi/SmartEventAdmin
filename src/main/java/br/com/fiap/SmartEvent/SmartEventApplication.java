5c2ca23f901bf77c80ffb4c7daadac13cee70f95
licciardi <lu.licciard@gmail.com>
2024-04-15 19:29:43 -0300

    Final

--- src/main/java/br/com/fiap/SmartEvent/SmartEventApplication.java
+++ src/main/java/br/com/fiap/SmartEvent/SmartEventApplication.java
@@ -10,12 +10,5 @@ public class SmartEventApplication {
 		SpringApplication.run(SmartEventApplication.class, args);
 	}
 
-	//Luigi Licciardi RM: 95776
-	//Vitor Santos Alves  RM: 96301
-	//
-	//
-	//Professora como vc havia dito estou informado que tive problema na alteração de Evento, mas especificamente no campo de alterar cidade aonde o "th:field" não estava funcionando.
-	//(eventos/editar-evento)
-	//(Campo de alterar cidade)
 
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,7 +2,6 @@
 
 INSERT INTO tb_cidade(nome,estado)VALUES('Iraquara', 'Bahia');
 INSERT INTO tb_cidade(nome,estado)VALUES('Souto Soares', 'Bahia');
-INSERT INTO tb_cidade(nome,estado)VALUES('Mulungu do Morro', 'Bahia');
 INSERT INTO tb_cidade(nome,estado)VALUES('Irece', 'Bahia');
 INSERT INTO tb_cidade(nome,estado)VALUES('São Paulo', 'São Paulo');
 INSERT INTO tb_cidade(nome,estado)VALUES('Mogi das Cruzes', 'São Paulo');
@@ -12,3 +11,4 @@ INSERT INTO tb_evento(nome,data,url,cidade_id)VALUES('Lollapalooza', '2024-03-6'
 INSERT INTO tb_evento(nome,data,url,cidade_id)VALUES('The Town', '2024-10-7', 'https://thetown.com.br/pt/',2);
 INSERT INTO tb_evento(nome,data,url,cidade_id)VALUES('Festa do Branco', '2024-10-7', 'https://www.festadobranco.com.br',3);
 INSERT INTO tb_evento(nome,data,url,cidade_id)VALUES('Rock in Rio', '2024-10-7', 'https://rockinrio.com/rio/pt-br/home/',4);
+INSERT INTO tb_evento(nome,data,url,cidade_id)VALUES('Rock in Bahia', '2024-10-7', 'https://rockinbahia.com/rio/pt-br/home/',1);
